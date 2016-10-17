22ff09325678c9c5d8f3b2e4f2bd21c215f6a6ad
Eivind Lindbråten <eivind.lindbraaten@gmail.com>
2016-10-17 08:12:35 +0200

    Added smooth scroll to internal links

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,19 @@
 // Smooth scrolling to the top of page
 function scrollToPosition(position, scrollDuration) {
 
+    var body = document.body,
+        html = document.documentElement;
+
+    var windowHeight = Math.max(body.scrollHeight, body.offsetHeight,
+        html.clientHeight, html.scrollHeight, html.offsetHeight);
+    var viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+    var topViewHeight = windowHeight - viewportHeight;
+
+    if (position > topViewHeight) {
+        position = topViewHeight;
+    }
+
     const currentScrollHeight = window.scrollY, // Number of pixels that the document is currently scrolled from the top.
         scrollStep = Math.PI / (scrollDuration / 15), // How long steps to take each turn.
         goToScrollHeight = position,
